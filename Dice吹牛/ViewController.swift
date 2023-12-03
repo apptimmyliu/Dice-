79f8beceae7a7fa4c1d93668c26ed7a95d54ac90
Timmy <timmyliu1997@hotmail.com>
2023-12-03 21:40:36 +0800

    Initial Commit

---
+++ Dice吹牛/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice吹牛
+//
+//  Created by Timmy on 2023/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
